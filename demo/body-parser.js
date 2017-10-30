3685bdaee80ccea39312862f2263d0268c7fa826
123 <23456@qq.com>
2017-10-30 19:57:20 +0800

    second

---
+++ demo/body-parser.js
@@ -0,0 +1,25 @@
+
+let queryString = require('querystring');
+
+exports.urlencoded = function () {
+
+    // 解析处理 post 数据
+    return function (req, res, next) {
+
+        let text = '';
+        // 接收post数据
+        req.on('data', (chunk) => {
+            text += chunk;
+        });
+
+        req.on('end', () => {
+            // console.log(text);
+
+            // console.log(queryString.parse(text));
+            // 解析post数据
+            req.body = queryString.parse(text);
+
+            next();
+        });
+    }
+}
